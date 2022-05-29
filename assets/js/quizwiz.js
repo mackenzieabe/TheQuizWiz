0a272313c5c40dbf357b4b300a45a066e3f23389
mackgabe <mackenzieabe@gmail.com>
2022-05-29 12:22:59 -0400

    added questions and choice buttons plus hidden feature once question is asked

--- assets/js/quizwiz.js
+++ assets/js/quizwiz.js
@@ -1,26 +1,34 @@
 var quiz = [
     {
-        question: "this is question 1",
-        choices: ['answer 1 of 1', 'answer 2 of 1', 'answer 3 of 1'],
-        correct: 'answer 1 of 1'
+        question: "What symbol is used to identiy classes in JavaScript?",
+        choices: ['!', '$', '.'],
+        correct: '.'
     },
     {
-        question: "this is question 2",
-        choices: ['answer 1 of 2', 'answer 2 of 2', 'answer 3 of 2'],
-        correct: 'answer 2 of 2'
+        question: "Arrays are enclosed with ______? ",
+        choices: ['parenthesis', 'curly brackets', 'square brackets'],
+        correct: 'square brackets'
     },
     {
-        question: "this is question 3",
-        choices: ['answer 1 of 3', 'answer 2 of 3', 'answer 3 of 3'],
-        correct: 'answer 3 of 3'
+        question: "What command do you use to create a new branch on gitbash?",
+        choices: ['I-command-you-to-create-a-new-branch-now', 'git checkout -b <newbranchname>', 'git create new branch'],
+        correct: 'git checkout -b <newbranchname>'
+
     }
 ]
+
+
 var score = 0
 var timer = 100
 var startContainer = document.querySelector('#start-container')
 var startBtn = document.querySelector('.start')
-var questionContainer = document.querySelector('.question-container')
-var choiceContainer = document.querySelector('.choice-container')
+var questionContainer = document.querySelector('#question-container')
+var questionContainer1=document.querySelector('.question-container1')
+var choiceContainer1 = document.querySelector('.choice-container1')
+var questionContainer2=document.querySelector('.question-container2')
+var choiceContainer2 = document.querySelector('.choice-container2')
+var questionContainer3=document.querySelector('.question-container3')
+var choiceContainer3 = document.querySelector('.choice-container3')
 
 startBtn.addEventListener('click', function() {
     startContainer.classList.add('hidden')
@@ -30,12 +38,12 @@ startBtn.addEventListener('click', function() {
 function renderQuestionOne() {
     var question = document.createElement('h1')
     question.textContent = quiz[0].question
-    questionContainer.append(question)
+    questionContainer1.append(question)
 
     for (var i = 0; i < quiz[0].choices.length; i++) {
         var choices = document.createElement('button')
         choices.textContent = quiz[0].choices[i]
-        choiceContainer.append(choices)
+        choiceContainer1.append(choices)
         
         choices.addEventListener('click', function(event) {
             var clicked = event.target.textContent
@@ -46,14 +54,60 @@ function renderQuestionOne() {
             } else {
                 console.log('incorrect');
                 timer -= 30
+
             }
-            renderQuestionTwo()
+            questionContainer1.classList.add('hidden')
+            choiceContainer1.classList.add('hidden')
+            renderQuestionTwo();
+            
         })
     }
 }
-
+ 
 function renderQuestionTwo() {
-    questionContainer.textContent = ""
-    choiceContainer.textContent = ""
-    
+    var question = document.createElement('h1')
+    question.textContent = quiz[1].question
+    questionContainer2.append(question)
+
+    for (var i = 0; i < quiz[1].choices.length; i++) {
+        var choices = document.createElement('button')
+        choices.textContent = quiz[1].choices[i]
+        choiceContainer2.append(choices)
+
+        choices.addEventListener('click',function(event) {
+            var clicked = event.target.textContent
+            if (clicked === quiz[1].correct) {
+                console.log('correct');
+                score += 33
+            
+            } else { 
+                console.log('incorrect');
+                timer -= 30
+            }
+            renderQuestionThree()
+        })
+    }
+} 
+function renderQuestionThree() {
+    var question = document.createElement('h1')
+    question.textContent = quiz[2].question
+    questionContainer3.append(question)
+
+    for (var i = 0; i < quiz[2].choices.length; i++) {
+        var choices = document.createElement('button')
+        choices.textContent = quiz[2].choices[i]
+        choiceContainer3.append(choices)
+
+        choices.addEventListener('click',function(event) {
+            var clicked = event.target.textContent
+            if (clicked === quiz[2].correct) {
+                console.log('correct');
+                score += 33
+            
+            } else { 
+                console.log('incorrect');
+                timer -= 30
+            }
+        })
+    }
 }
