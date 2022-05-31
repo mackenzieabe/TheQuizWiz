0e400f1cd8d5fca8e76b3a62a7b8ee3895d3b3be
mackgabe <mackenzieabe@gmail.com>
2022-05-31 15:02:10 -0400

    finished buttons and now working on css and adding boxes

--- assets/js/quizwiz.js
+++ assets/js/quizwiz.js
@@ -13,30 +13,41 @@ var quiz = [
         question: "What command do you use to create a new branch on gitbash?",
         choices: ['I-command-you-to-create-a-new-branch-now', 'git checkout -b <newbranchname>', 'git create new branch'],
         correct: 'git checkout -b <newbranchname>'
-
     }
 ]
 
 
 var score = 0
-var timer = 100
+var timer = 30
+var questionIndex = 0
+var quizContainer = document.querySelector('#quiz-container')
 var startContainer = document.querySelector('#start-container')
+var timeContainer = document.querySelector('.time-container')
 var buttonContainer = document.querySelector('#button-container')
 var startBtn = document.querySelector('.start')
-var nextBtn = document.querySelector('.next')
-var submitBtn = document.querySelector('.submit')
-var questionContainer = document.querySelector('#question-container')
-var questionContainer1=document.querySelector('.question-container1')
+var nextBtn = document.querySelector('.next-btn')
+var questionContainer1 = document.querySelector('.question-container1')
 var choiceContainer1 = document.querySelector('.choice-container1')
-var questionContainer2=document.querySelector('.question-container2')
-var choiceContainer2 = document.querySelector('.choice-container2')
-var questionContainer3=document.querySelector('.question-container3')
-var choiceContainer3 = document.querySelector('.choice-container3')
 
-startBtn.addEventListener('click', function() {
+startBtn.addEventListener('click', function () {
     startContainer.classList.add('hidden')
     renderQuestionOne()
+    startTimer()
 })
+
+function startTimer() {
+    timeContainer.textContent = timer
+    var timeInt = setInterval(function () {
+        timer--
+        timeContainer.textContent = timer
+        if (timer <= 0 || questionIndex === quiz.length) {
+            endQuiz()
+            clearInterval(timeInt)
+            timeContainer.textContent = "0"
+        }
+    }, 1000);
+}
+
 function renderQuestionOne() {
     var question = document.createElement('h1')
     question.textContent = quiz[0].question
@@ -46,8 +57,8 @@ function renderQuestionOne() {
         var choices = document.createElement('button')
         choices.textContent = quiz[0].choices[i]
         choiceContainer1.append(choices)
-        
-        choices.addEventListener('click', function(event) {
+
+        choices.addEventListener('click', function (event) {
             var clicked = event.target.textContent
             if (clicked === quiz[0].correct) {
                 console.log('correct');
@@ -56,70 +67,95 @@ function renderQuestionOne() {
             } else {
                 console.log('incorrect');
                 timer -= 30
-
             }
-            questionContainer1.classList.add('hidden')
-            choiceContainer1.classList.add('hidden')
-
-            nextBtn.addEventListener('click',function(event){
-                if (event.key === "Next"){
-                    event.preventDefault();
-                    document.getElementsByClassName('next').click();
-                }
-                renderQuestionTwo();
-            })
-            
-            
-        })
+            renderQuestionTwo();
+        });
     }
 }
- 
+
+
 function renderQuestionTwo() {
+    questionIndex++
+    questionContainer1.textContent = ""
+    choiceContainer1.textContent = ""
     var question = document.createElement('h1')
     question.textContent = quiz[1].question
-    questionContainer2.append(question)
+    questionContainer1.append(question)
 
     for (var i = 0; i < quiz[1].choices.length; i++) {
         var choices = document.createElement('button')
         choices.textContent = quiz[1].choices[i]
-        choiceContainer2.append(choices)
+        choiceContainer1.append(choices)
 
-        choices.addEventListener('click',function(event) {
+        choices.addEventListener('click', function (event) {
             var clicked = event.target.textContent
             if (clicked === quiz[1].correct) {
                 console.log('correct');
                 score += 33
-            
-            } else { 
+
+            } else {
                 console.log('incorrect');
                 timer -= 30
             }
-            questionContainer2.classList.add('hidden')
-            choiceContainer2.classList.add('hidden')
+
             renderQuestionThree()
         })
     }
-} 
+}
 function renderQuestionThree() {
+    questionIndex++
+    questionContainer1.textContent = ""
+    choiceContainer1.textContent = ""
     var question = document.createElement('h1')
     question.textContent = quiz[2].question
-    questionContainer3.append(question)
+    questionContainer1.append(question)
 
     for (var i = 0; i < quiz[2].choices.length; i++) {
         var choices = document.createElement('button')
         choices.textContent = quiz[2].choices[i]
-        choiceContainer3.append(choices)
+        choiceContainer1.append(choices)
 
-        choices.addEventListener('click',function(event) {
+        choices.addEventListener('click', function (event) {
             var clicked = event.target.textContent
             if (clicked === quiz[2].correct) {
                 console.log('correct');
                 score += 33
-            
-            } else { 
+
+            } else {
                 console.log('incorrect');
                 timer -= 30
             }
+            questionIndex++
         })
     }
+}
+
+
+function endQuiz() {
+    questionContainer1.textContent = ""
+    choiceContainer1.textContent = ""
+    var name = document.createElement('input')
+    name.setAttribute('placeholder', 'Please Type Your Name')
+    questionContainer1.append(name)
+
+    var submitBtn = document.createElement('button')
+    submitBtn.textContent = 'Submit'
+    questionContainer1.append(submitBtn)
+
+    submitBtn.addEventListener('click', function () {
+        var userData = {
+            name: name.value,
+            finalScore: score
+        }
+
+        var storage = JSON.parse(localStorage.getItem('quizScores'))
+        if (storage === null) {
+            storage = []
+        }
+
+        storage.push(userData)
+        localStorage.setItem('quizScores', JSON.stringify(storage))
+        window.location.href = 'highscores.html'
+    })
+
 }
